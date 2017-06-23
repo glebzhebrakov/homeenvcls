b812d9a8570b89e181e18ff7862b8ae2c1ccf602
Gleb Zhebrakov <gzhebrakov@sciforce.solutions>
2017-06-23 18:40:23 +0300

    revert back to stable

--- app.py
+++ app.py
@@ -1,26 +1,11 @@
 # import threading
 #
-
-from flask import Flask
-
-
 import log
 logger = log.setup_custom_logger('homeenvcls')
 logger.info('#################### starting homeenv classification worker###########################')
 
-
-
-#
 from messaging.rabbit import begin_consuming
-from service.classificationService import classify_image
-from web.controller import configure
 
 begin_consuming()
-# rabbitThread = threading.Thread(target=begin_consuming)
-# rabbitThread.start()
-#
-# app = Flask(__name__)
-#
-# configure(app).run()
 
 
